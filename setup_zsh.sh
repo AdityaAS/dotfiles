024c22bb7d7488a9287c8db7665bf7f058bf4423
Aditya Sarma <asaditya1195@gmail.com>
2023-05-25 15:16:16 -0400

    Update setup_zsh

--- setup_zsh.sh
+++ setup_zsh.sh
@@ -1,9 +1,9 @@
 echo "Home directory is: $HOME"
 
 # Setup ohmyzsh first
-wget https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh
-rm -rf .oh-my-zsh
-sh install.sh
+wget -P $HOME/ https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh
+rm -rf $HOME/.oh-my-zsh
+sh $HOME/install.sh
 git clone https://github.com/zsh-users/zsh-autosuggestions $HOME/.oh-my-zsh/plugins/zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $HOME/.oh-my-zsh/plugins/zsh-syntax-highlighting
 
